495cd385142e489502cc3ae8df185037b9f17252
npcraig <npcraig@gmail.com>
2024-06-24 17:48:41 -0400

    updated links

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 function openLinksInNewTabs() {
-    const selectors = [
-      'body > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > a',
-      'body > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > a',
-      'body > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > a',
-      'body > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div:nth-child(3) > table > tbody > tr:nth-child(4) > td:nth-child(2) > a'
-    ];
+  const selectors = [
+    'body > md-content > md-content > div.layout-column.flex > md-content > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > a',
+    'body > md-content > md-content > div.layout-column.flex > md-content > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div:nth-child(3) > table > tbody > tr:nth-child(4) > td:nth-child(2) > a',
+    'body > md-content > md-content > div.layout-column.flex > md-content > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > a',      
+    'body > md-content > md-content > div.layout-column.flex > md-content > acc-main-container > main > div > div > div.transaction-detail > div:nth-child(2) > div.summaryPortlet > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div > div > div.row.portlet-body-wrapper > div > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > a'
+  ];
   
     selectors.forEach(selector => {
       const link = document.querySelector(selector);
